00104b6856ae5ad16f7c4d5d5e93154f523cb8f8
IamSinha27 <79939540+IamSinha27@users.noreply.github.com>
2021-08-11 20:04:20 +0530

    Sponsors page template
    Added a sponsors page template

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -33,44 +33,55 @@ function Resume() {
       <Particle />
       <Container>
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
+          {/* <Button variant="primary" href={pdf} target="_blank">
             <AiOutlineDownload />
             &nbsp;Download CV
-          </Button>
+          </Button> */}
+          <h1>OUR  <strong className="purple">SPONSORS</strong></h1>
         </Row>
         <Row className="resume">
           <Col md={6} className="resume-left">
-            <h3 className="resume-title">Experience</h3>
-            <Resumecontent
+            <h3 className="resume-title">Sponsor 1</h3>
+            {/* <Resumecontent
               title="JUNIOR ML ENGINEER [Omdena]"
               date="June 2020 - August 2020"
               content={[
                 "Assembled the data from various social media platforms using Twitter, Reddit.Interpreted the collected text using word-clouds and various other factors that affect the change of sentiments of youth.",
                 " Utilized the data to find the issues using Topic Modelling and Building models using LSTM, BERT to predict the sentiments of the youth.",
-              ]}
-            />
-            <h3 className="resume-title">Extracurricular Activities</h3>
-            <Resumecontent
+              ]} */}
+              <h1>GITHUB</h1>
+               <h3>image/logo</h3>
+
+            
+            <h3 className="resume-title">Sponsor 2</h3>
+            {/* <Resumecontent
               title="Web Developer [Pantheon-2019 Technical Fest of BIT Mesra]"
               content={[
                 "Worked on creating the frontend-end of the website using Bootstrap, Javascript.",
               ]}
-            />
-            <Resumecontent
-              title="Web Developer [Bitotsav-2020 Technical Fest of BIT Mesra]"
+            /> */}
+            <h1>GEEKS FOR GEEKS </h1>
+            <h3>image/logo</h3>
+           
+            {/* <Resumecontent
+              title="HACKERRANK"
               content={[
                 "Operated on developing the frontend end of the website using Bootstrap, Javascript and backend APIs using Node.js",
-              ]}
-            />
+              ]} */}
+              {/* <h1>HACKERRANK</h1>
+              <h3>image/logo</h3> */}
+            {/* /> */}
           </Col>
           <Col md={6} className="resume-right">
-            <h3 className="resume-title">Education</h3>
-            <Resumecontent
+            <h3 className="resume-title">Sponsor 3</h3>
+            {/* <Resumecontent
               title="IMSC MATHS AND COMPUTING [BIT Mesra, Ranchi] "
               date="2018 - Present"
               content={[`CGPA: ${cgpa} (Till ${sem}th Sem)`]}
-            />
-            <Resumecontent
+            /> */}
+            <h1>DevFolio</h1>
+            <h3>image/logo</h3>
+            {/* <Resumecontent
               title="12TH BOARD [ODM Public School,Odisha]"
               date="2015 - 2017"
               content={["Precentage: 88%"]}
@@ -79,9 +90,9 @@ function Resume() {
               title="10TH BOARD [ST Mary's School,Odisha] "
               date="2005 - 2015"
               content={["Precentage: 86%"]}
-            />
-            <h3 className="resume-title">Ranks and Achivements</h3>
-            <Resumecontent
+            /> */}
+            <h3 className="resume-title">Sponsor 4</h3>
+            {/* <Resumecontent
               title=""
               content={[
                 `Current rank in Spoj ${spojRank}`,
@@ -89,13 +100,15 @@ function Resume() {
                 "Top Performer in Code-Break 1.0",
                 "Participant in Hack-A-Bit 2019",
               ]}
-            />
+            /> */}
+            <h1>HACKERRANK</h1>
+            <h3>image/logo</h3>
           </Col>
         </Row>
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
+          {/* <Button variant="primary" href={pdf} target="_blank">
           <AiOutlineDownload />&nbsp;Download CV
-          </Button>
+          </Button> */}
         </Row>
       </Container>
     </Container>
